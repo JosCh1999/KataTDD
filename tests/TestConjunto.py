b0a3a3b280a7f74f8d547ec07a01f67a3883e9a4
JosCh1999 <75557750@continental.edu.pe>
2022-09-19 18:39:13 -0500

    Caso 1: Calcular Promedio nulo

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -3,8 +3,9 @@ from src.logica.Conjunto import Conjunto
 
 
 class TestConjunto(unittest.TestCase):
-    pass
-
+    def test_conjunto_vacio_retornaNone(self):
+        conjunto=Conjunto({})
+        self.assertIsNone(conjunto.promedio())
 
 
 
